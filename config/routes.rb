adbea2f0201c9c3806b8d1fffcf7491355523a57
Jon <morganusn@gmail.com>
2017-02-16 22:48:34 +0000

    fixed route to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+  root 'products#index'
+
+
+
 end
 
 
